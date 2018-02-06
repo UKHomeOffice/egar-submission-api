b888db8cf51e223c7ff67195a44908008136b36d
Bill Hart <32137104+billhartcivica@users.noreply.github.com>
2018-02-06 16:41:01 +0000

    Update docker-deploy.sh

--- docker-deploy.sh
+++ docker-deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NAME="${1}"
-version=$(./utils/get-site-version.sh)
+version=$(./scripts/bash/get-site-version.sh)
 
 docker login -u="ukhomeofficedigital+egar_robot" -p=${DOCKER_PASSWORD} quay.io
 docker tag $NAME:$version quay.io/ukhomeofficedigital/$NAME:$version
